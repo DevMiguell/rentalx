79125b33f476bba7ac6a1245dcaf074090e12637
Fernando Miguel <buguel2000@gmail.com>
2022-04-17 08:39:20 -0300

    feat: Specification case

--- src/modules/cars/services/CreateSpecificationService.ts
+++ src/modules/cars/services/CreateSpecificationService.ts
@@ -1,6 +1,25 @@
+import { ISpecificationRepository } from "../repositories/ISpecificationRepository";
+
+interface IResquest {
+  name: string;
+  description: string;
+}
+
 class CreateSpecificationService {
-  execute() {
-    console.log("CreateSpecificationService");
+  constructor(private specificationRepository: ISpecificationRepository) {}
+
+  execute({ name, description }: IResquest): void {
+    const specificationAlreadyExists =
+      this.specificationRepository.findByName(name);
+
+    if (specificationAlreadyExists) {
+      throw new Error("Specification not implemented.");
+    }
+
+    this.specificationRepository.create({
+      description,
+      name,
+    });
   }
 }
 
